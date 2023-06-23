cf3090ca5728d568da0ea6409051e5052784f05b
heejin_song <song_heejin@naver.com>
2023-06-23 13:29:02 +0900

    map setting

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { useState } from 'react';
 
-
 function App() {
 
+  const initialState = useSelector((state) => {
+    return state.todolist.todolist
+  })
+
+  const [todos, setTodos] = useState(initialState)
 
 
   return (
@@ -13,7 +17,10 @@ function App() {
       <h2>My Todo List</h2>
     </div>
 
-    <div>
+    <div style={{
+          backgroundColor: "orange",
+          padding: "10px",
+        }}>
       <form>
         제목 : <input type="text" placeholder='제목' />
         내용 : <input type="text" placeholder='내용' />
@@ -23,7 +30,17 @@ function App() {
 
     <div>
       <h2>Working...🔥</h2>
+      {todos.map(function (todo) {
+        return (
+          <div key={todo.id}>
+            <p>{todo.id}</p>
+          </div>
+        )
+      })
+      }
+    </div>
 
+    <div>
       <h2>Done...!😄</h2>
     </div>
     </>
--- src/redux/modules/todolist.js
+++ src/redux/modules/todolist.js
@@ -1,13 +1,19 @@
-import {uuid} from "react"
 
 const initialState = {
-    todolist: {
-        id: uuid,
+    todolist: [
+    {
+        id: 1,
         title: "리엑트 공부하기",
         content: "강의 완주!",
         isDone: false,
     },
-}
+    {
+        id: 2,
+        title: "리엑트 공부하기",
+        content: "강의 완주!",
+        isDone: false,
+    },
+]}
 
 const todolist = (state = initialState, action) => {
     switch (action.type) {
